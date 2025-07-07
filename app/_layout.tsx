c68c2789b118bde98befbc93ed05b87e667bf779
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-07-07 19:22:31 +0300

    Assets Update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,17 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
 import './global.css';
 
+
 export default function RootLayout() {
+  const [] = useFonts({
+    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
   return <Stack />;
 }
