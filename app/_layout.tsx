c28dae95271dd0768fe5a6a4de3343a6819c205e
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-07-07 23:54:23 +0300

    Home Screen UI
    - Started Home Screen UI (not finished).

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 
@@ -6,12 +6,18 @@ import './global.css';
 
 
 export default function RootLayout() {
-  const [] = useFonts({
+  const [fonstLoaded, error] = useFonts({
     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
     "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
     "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
     "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
   });
-  return <Stack />;
+
+  useEffect(() => {
+    if(error) throw error;
+    if(fonstLoaded) SplashScreen.hideAsync();
+  }, [fonstLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,35 @@
-import { Text, View } from "react-native";
-import "./global.css";
+import { offers } from "@/constants";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList 
+        data={offers} 
+        renderItem={({ item, index }) => {
+          return (
+            <View>
+              <Pressable className="offer-card" style={{ backgroundColor: item.color }}>
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className="h-full w-1/2">
+                      <Image source={item.image} className="size-full" resizeMode="contain"/>
+                    </View>
+
+                    <View className="offer-card__info">
+                      <Text>
+                        {item.title}
+                      </Text>
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          )
+        }}
+      />
+    </SafeAreaView>
   );
 }
