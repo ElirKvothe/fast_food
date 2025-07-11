4ecbea8518edca351a61de5e21e2f7f079abd913
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-07-11 23:01:14 +0300

    Components
    - CustomButton and CustomInput component created.
    - Sign in UI finished.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    platform: "com.jsm.foodordering",
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-}
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.jsm.foodordering",
+  databaseId: "686fe3be0025a509f9c3",
+  userCollectionId: "686fe412000f74f77779",
+};
