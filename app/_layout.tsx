16792e444d2075cb203d865f29f9a9958201e5b0
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-07-12 20:28:26 +0300

    Zustand and Auth Functionality
    - Implementing Zustand.
    - Adding functionality to Auth page.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 
 import './global.css';
+import useAuthStore from "@/store/auth.store";
 
 
 export default function RootLayout() {
+  const {isLoading, fetchAuthenticatedUser} = useAuthStore();
+
   const [fonstLoaded, error] = useFonts({
     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
@@ -18,6 +21,12 @@ export default function RootLayout() {
     if(error) throw error;
     if(fonstLoaded) SplashScreen.hideAsync();
   }, [fonstLoaded, error]);
+
+  useEffect(() => {
+    fetchAuthenticatedUser();
+  }, []);
+
+  if(!fonstLoaded || isLoading) return null;
 
   return <Stack screenOptions={{ headerShown: false }}/>;
 }
